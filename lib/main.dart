81ded9d70a9464edafea460b6070a967ec5ccb16
parth-iroid <parthmori.iroidsolutions@gmail.com>
2024-01-16 17:00:57 +0530

    firebase changes

--- lib/main.dart
+++ lib/main.dart
@@ -35,12 +35,14 @@ Future<void> main() async {
         ],
       );
       await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: 'AIzaSyAEPe7upukbefIVDSmvNL90bc-MYfObF4I',
-            appId: '1:568471335476:android:870d2cdcbf08727af09061',
-            messagingSenderId: '568471335476',
-            projectId: 'grape-market-place'),
-      ).catchError((e) {
+
+              // options: const FirebaseOptions(
+              //     apiKey: 'AIzaSyAEPe7upukbefIVDSmvNL90bc-MYfObF4I',
+              //     appId: '1:568471335476:android:870d2cdcbf08727af09061',
+              //     messagingSenderId: '568471335476',
+              //     projectId: 'grape-market-place'),
+              )
+          .catchError((e) {
         print(e);
         return e;
       }).then((v) async {
