17ec976757aa1ad77f1ed4185857bf65f5642c81
parth-iroid <parthmori.iroidsolutions@gmail.com>
2024-01-19 10:48:10 +0530

    commit chabges

--- lib/presentation/main/tabs/profile/profile_view.dart
+++ lib/presentation/main/tabs/profile/profile_view.dart
@@ -7,6 +7,7 @@ import 'package:grape_customer_app/domain/core/png_image_constants.dart';
 import 'package:grape_customer_app/domain/core/string_constant.dart';
 import 'package:grape_customer_app/domain/core/svg_image_constants.dart';
 import 'package:grape_customer_app/presentation/common/widgets/base_text.dart';
+import 'package:grape_customer_app/presentation/core/app_router.gr.dart';
 import 'package:grape_customer_app/presentation/core/styles/styles.dart';
 import 'package:grape_customer_app/presentation/core/widgets/layout/layout.dart';
 
