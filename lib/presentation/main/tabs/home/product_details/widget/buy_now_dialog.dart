bdd30ad00915212c3f4d4b7a10c3fd81ff2c0528
maitry-iroid <maitry@iroidsolutions.com>
2024-02-06 19:27:25 +0530

    maitry : minor fix

---
+++ lib/presentation/main/tabs/home/product_details/widget/buy_now_dialog.dart
@@ -0,0 +1,355 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:grape_customer_app/domain/core/math_utils.dart';
+import 'package:grape_customer_app/presentation/common/widgets/base_text.dart';
+import 'package:grape_customer_app/presentation/core/styles/app_colors.dart';
+import 'package:grape_customer_app/presentation/core/widgets/buttons/common_button.dart';
+
+class BuyNowDialog extends StatelessWidget {
+  const BuyNowDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+  buyNowDialog(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      backgroundColor: AppColors.white,
+      elevation: 0,
+      useSafeArea: true,
+      scrollControlDisabledMaxHeightRatio: 0.75,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(getSize(14)),
+          topRight: Radius.circular(getSize(14)),
+        ),
+      ),
+      builder: (context) => SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: getSize(20),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    //  alignment: Alignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: getSize(60),
+                            width: getSize(60),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color:
+                                    AppColors.primaryOrange.withOpacity(0.20),
+                              ),
+                              borderRadius: BorderRadius.circular(getSize(6)),
+                              image: DecorationImage(
+                                image: CachedNetworkImageProvider(
+                                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0U3avlAFpuN9Sf5PVhN3MHdXQzh6rJusL93tMQRngAnrK1k0Z9CH4hzhArR0kyV-Fm_E&usqp=CAU'),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: getSize(12),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              BaseText(
+                                text: 'Nothing Phone 1',
+                                fontWeight: FontWeight.w600,
+                                textColor: AppColors.black.withOpacity(0.80),
+                              ),
+                              SizedBox(
+                                height: getSize(8),
+                              ),
+                              Row(
+                                children: [
+                                  BaseText(
+                                    text: '\$350',
+                                    textDecoration: TextDecoration.lineThrough,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                    textColor: AppColors.black.withOpacity(0.4),
+                                  ),
+                                  SizedBox(
+                                    width: getSize(6),
+                                  ),
+                                  BaseText(
+                                    text: '\$350',
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    textColor: Color(0xFF527FF2),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          context.router.pop();
+                        },
+                        child: Align(
+                          alignment: Alignment.topRight,
+                          child: Icon(Icons.close_rounded),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: getSize(16),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: Divider(
+                color: AppColors.black.withOpacity(0.1),
+                height: 0,
+              ),
+            ),
+            SizedBox(
+              height: getSize(16),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: BaseText(
+                text: 'Colors:',
+                textColor: AppColors.black.withOpacity(0.7),
+              ),
+            ),
+            SizedBox(
+              height: getSize(8),
+            ),
+            Flexible(
+              child: ListView.builder(
+                itemCount: 10,
+                shrinkWrap: true,
+                padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+                scrollDirection: Axis.horizontal,
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    Container(
+                      height: getSize(60),
+                      width: getSize(60),
+                      margin: EdgeInsets.only(
+                        left: index != 0 ? getSize(7) : 0,
+                        right: index == 9 ? 0 : getSize(7),
+                      ),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: index == 0
+                            ? AppColors.primaryOrange.withOpacity(0.20)
+                            : AppColors.white,
+                        borderRadius: BorderRadius.circular(getSize(6)),
+                        border: Border.all(
+                          color: index == 0
+                              ? AppColors.primaryOrange
+                              : AppColors.black.withOpacity(0.20),
+                        ),
+                      ),
+                      child: Container(
+                        height: getSize(38),
+                        width: getSize(38),
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: AppColors.black.withOpacity(0.10),
+                              offset: Offset(0, 0),
+                              blurRadius: 14,
+                            ),
+                          ],
+                          color: index == 0 ? AppColors.black : AppColors.white,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: getSize(6),
+                    ),
+                    BaseText(
+                      text: 'Black',
+                      fontSize: 10,
+                      fontWeight: FontWeight.w500,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: getSize(16),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: Divider(
+                color: AppColors.black.withOpacity(0.1),
+                height: 0,
+              ),
+            ),
+            SizedBox(
+              height: getSize(16),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: BaseText(
+                text: 'Size:',
+                textColor: AppColors.black.withOpacity(0.7),
+              ),
+            ),
+            SizedBox(
+              height: getSize(8),
+            ),
+            Flexible(
+              child: ListView.builder(
+                itemCount: 10,
+                shrinkWrap: true,
+                padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+                scrollDirection: Axis.horizontal,
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (context, index) => Container(
+                  margin: EdgeInsets.only(
+                    left: index != 0 ? getSize(7) : 0,
+                    right: index == 9 ? 0 : getSize(7),
+                  ),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: getSize(14),
+                    vertical: getSize(12),
+                  ),
+                  decoration: BoxDecoration(
+                    color: index == 0
+                        ? AppColors.primaryOrange.withOpacity(0.20)
+                        : AppColors.white,
+                    borderRadius: BorderRadius.circular(getSize(6)),
+                    border: Border.all(
+                      color: index == 0
+                          ? AppColors.primaryOrange
+                          : AppColors.black.withOpacity(0.20),
+                    ),
+                  ),
+                  child: BaseText(
+                    text: '128 GB',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: getSize(16),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: Divider(
+                color: AppColors.black.withOpacity(0.1),
+                height: 0,
+              ),
+            ),
+            SizedBox(
+              height: getSize(16),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: getSize(18)),
+              decoration: BoxDecoration(
+                color: Color(0xFFD9D9D9).withOpacity(0.20),
+                borderRadius: BorderRadius.circular(getSize(10)),
+              ),
+              padding: EdgeInsets.symmetric(
+                horizontal: getSize(20),
+                vertical: getSize(12),
+              ),
+              child: Row(
+                children: [
+                  BaseText(
+                    text: 'Quantity',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  Spacer(),
+                  Container(
+                    height: getSize(20),
+                    width: getSize(20),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: AppColors.white,
+                      borderRadius: BorderRadius.circular(getSize(6)),
+                      border: Border.all(
+                        color: AppColors.black.withOpacity(0.20),
+                      ),
+                    ),
+                    child: Icon(
+                      Icons.remove,
+                      size: getSize(12),
+                    ),
+                  ),
+                  SizedBox(
+                    width: getSize(8),
+                  ),
+                  Container(
+                    height: getSize(27),
+                    width: getSize(27),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryOrange,
+                      shape: BoxShape.circle,
+                    ),
+                    child: BaseText(
+                      text: '01',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      textColor: AppColors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    width: getSize(8),
+                  ),
+                  Container(
+                    height: getSize(20),
+                    width: getSize(20),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: AppColors.white,
+                      borderRadius: BorderRadius.circular(getSize(6)),
+                      border: Border.all(
+                        color: AppColors.black.withOpacity(0.20),
+                      ),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: getSize(12),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: getSize(30),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: CommonButton(
+                onPressed: () {},
+                buttonText: 'Buy Now',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
