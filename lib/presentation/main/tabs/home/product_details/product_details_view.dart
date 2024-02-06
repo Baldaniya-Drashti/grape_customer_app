60ec79cf4f2c2391dcef586b710fd96ef5d2f0d7
maitry-iroid <maitry@iroidsolutions.com>
2024-02-06 13:21:08 +0530

    maitry : product screen done

--- lib/presentation/main/tabs/home/product_details/product_details_view.dart
+++ lib/presentation/main/tabs/home/product_details/product_details_view.dart
@@ -44,125 +44,142 @@ class ProductDetailsView extends StatelessWidget {
       body: SafeArea(
         child: ListView(
           shrinkWrap: true,
-          padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+          // padding: EdgeInsets.symmetric(horizontal: getSize(18)),
           physics: BouncingScrollPhysics(),
           children: [
-            CachedNetworkImage(
-              imageUrl:
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0U3avlAFpuN9Sf5PVhN3MHdXQzh6rJusL93tMQRngAnrK1k0Z9CH4hzhArR0kyV-Fm_E&usqp=CAU',
-              height: getSize(216),
-              placeholder: (context, url) => Container(
+            Center(
+              child: CachedNetworkImage(
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0U3avlAFpuN9Sf5PVhN3MHdXQzh6rJusL93tMQRngAnrK1k0Z9CH4hzhArR0kyV-Fm_E&usqp=CAU',
                 height: getSize(216),
-                color: Colors.grey.shade300,
+                placeholder: (context, url) => Container(
+                  height: getSize(216),
+                  color: Colors.grey.shade300,
+                ),
               ),
+            ),
+            SizedBox(
+              height: getSize(10),
             ),
             SizedBox(
               height: getSize(50),
               child: SelectModelWidget(),
             ),
-            Row(
-              children: [
-                BaseText(
-                  text: 'Nothing Phone 1',
-                  fontWeight: FontWeight.w600,
-                ),
-                SizedBox(
-                  width: getSize(4),
-                ),
-                Container(
-                  height: getSize(16),
-                  width: getSize(36),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFFFF094),
-                    borderRadius: BorderRadius.circular(
-                      getSize(4),
+            SizedBox(
+              height: getSize(20),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: Row(
+                children: [
+                  BaseText(
+                    text: 'Nothing Phone 1',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  SizedBox(
+                    width: getSize(4),
+                  ),
+                  Container(
+                    height: getSize(16),
+                    width: getSize(36),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFF094),
+                      borderRadius: BorderRadius.circular(
+                        getSize(4),
+                      ),
+                    ),
+                    child: BaseText(
+                      text: '-20%',
+                      fontSize: 10,
+                      fontWeight: FontWeight.w500,
+                      textColor: Color(0xFFFF770E),
                     ),
                   ),
-                  child: BaseText(
-                    text: '-20%',
-                    fontSize: 10,
-                    fontWeight: FontWeight.w500,
-                    textColor: Color(0xFFFF770E),
+                  Spacer(),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: getSize(14),
+                      vertical: getSize(4),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF666666),
+                      borderRadius: BorderRadius.circular(getSize(6)),
+                    ),
+                    child: BaseText(
+                      text: 'Out of Stock',
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                      textColor: AppColors.white,
+                    ),
                   ),
-                ),
-                Spacer(),
-                Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.symmetric(
-                    horizontal: getSize(14),
-                    vertical: getSize(4),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: Row(
+                children: [
+                  CommonRatingBar(
+                    onRatingUpdate: (p0) {},
+                    iconnSize: getSize(12),
+                    initialRating: 4.5,
+                    horizontalPadding: getSize(4),
+                    absorbing: true,
                   ),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF666666),
-                    borderRadius: BorderRadius.circular(getSize(6)),
+                  SizedBox(
+                    width: getSize(4),
                   ),
-                  child: BaseText(
-                    text: 'Out of Stock',
+                  BaseText(
+                    text: '200 Sold',
                     fontSize: 12,
                     fontWeight: FontWeight.w500,
-                    textColor: AppColors.white,
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                CommonRatingBar(
-                  onRatingUpdate: (p0) {},
-                  iconnSize: getSize(12),
-                  initialRating: 4.5,
-                  horizontalPadding: getSize(4),
-                  absorbing: true,
-                ),
-                SizedBox(
-                  width: getSize(4),
-                ),
-                BaseText(
-                  text: '200 Sold',
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                ),
-                Spacer(),
-                BaseText(
-                  text: '\$350',
-                  textDecoration: TextDecoration.lineThrough,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                  textColor: AppColors.black.withOpacity(0.4),
-                ),
-                SizedBox(
-                  width: getSize(6),
-                ),
-                BaseText(
-                  text: '\$350',
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  textColor: Color(0xFF527FF2),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                SvgPicture.asset(
-                  SvgImageConstant.location,
-                  height: getSize(14),
-                  width: getSize(14),
-                  colorFilter: ColorFilter.mode(
-                    AppColors.black.withOpacity(0.7),
-                    BlendMode.srcIn,
+                  Spacer(),
+                  BaseText(
+                    text: '\$350',
+                    textDecoration: TextDecoration.lineThrough,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    textColor: AppColors.black.withOpacity(0.4),
                   ),
-                ),
-                SizedBox(
-                  width: getSize(2),
-                ),
-                BaseText(
-                  text: 'Hyderabad',
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                  textColor: AppColors.black.withOpacity(0.7),
-                ),
-              ],
+                  SizedBox(
+                    width: getSize(6),
+                  ),
+                  BaseText(
+                    text: '\$350',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    textColor: Color(0xFF527FF2),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    SvgImageConstant.location,
+                    height: getSize(14),
+                    width: getSize(14),
+                    colorFilter: ColorFilter.mode(
+                      AppColors.black.withOpacity(0.7),
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                  SizedBox(
+                    width: getSize(2),
+                  ),
+                  BaseText(
+                    text: 'Hyderabad',
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    textColor: AppColors.black.withOpacity(0.7),
+                  ),
+                ],
+              ),
             ),
             SizedBox(
               height: getSize(20),
@@ -207,53 +224,67 @@ class ProductDetailsView extends StatelessWidget {
             SizedBox(
               height: getSize(20),
             ),
-            BaseText(
-              text: 'Product Specification:',
-              fontSize: 14,
-              fontWeight: FontWeight.w600,
-              textColor: AppColors.black.withOpacity(0.8),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: BaseText(
+                text: 'Product Specification:',
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+                textColor: AppColors.black.withOpacity(0.8),
+              ),
             ),
             SizedBox(
               height: getSize(10),
             ),
-            BaseText(
-              text:
-                  'Glyph Interface Meet the Glyph Interface. A new way to communicate. Unique light patterns indicate whos calling. Signals app notifications, charging status and more. Everyday interactions, made joyful. Design Design that goes beyond the surface. Dual-side Gorilla? Glass gives toughness. Advanced vibration motors make touch responses life-like. The symmetrical bezels and aluminium frame adds elegance, lightness and durabilit 50MP Dual camera Time to unlearn. Better quality isnt more cameras. This extraordinary dual camera has two advanced 50 MP sensors. For brilliant realism. Shoots exceptionally stable videos. Has studio lighting built-in. And Night Mode to take images to the next level. Display 1 billion colours. Every hue, powerfully true-to-life. 6.55? OLED display. HDR10+. Richer colour and deeper contrasts, tuned to each scene. Adaptive 120Hz refresh rate for irresistibly responsive interactions, while being reassuringly power-efficient.',
-              fontSize: 12,
-              showFullDescription: true,
-              textColor: AppColors.black.withOpacity(0.6),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: BaseText(
+                text:
+                    'Glyph Interface Meet the Glyph Interface. A new way to communicate. Unique light patterns indicate whos calling. Signals app notifications, charging status and more. Everyday interactions, made joyful. Design Design that goes beyond the surface. Dual-side Gorilla? Glass gives toughness. Advanced vibration motors make touch responses life-like. The symmetrical bezels and aluminium frame adds elegance, lightness and durabilit 50MP Dual camera Time to unlearn. Better quality isnt more cameras. This extraordinary dual camera has two advanced 50 MP sensors. For brilliant realism. Shoots exceptionally stable videos. Has studio lighting built-in. And Night Mode to take images to the next level. Display 1 billion colours. Every hue, powerfully true-to-life. 6.55? OLED display. HDR10+. Richer colour and deeper contrasts, tuned to each scene. Adaptive 120Hz refresh rate for irresistibly responsive interactions, while being reassuringly power-efficient.',
+                fontSize: 12,
+                showFullDescription: true,
+                textColor: AppColors.black.withOpacity(0.6),
+              ),
             ),
             SizedBox(
               height: getSize(20),
             ),
-            Row(
-              children: [
-                BaseText(
-                  text: 'Review Product',
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
-                ),
-                Spacer(),
-                BaseText(
-                  text: 'See More',
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                ),
-                Icon(
-                  Icons.arrow_forward_ios_rounded,
-                  size: getSize(18),
-                )
-              ],
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+              child: Row(
+                children: [
+                  BaseText(
+                    text: 'Review Product',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  Spacer(),
+                  BaseText(
+                    text: 'See More',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    size: getSize(18),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: getSize(5),
             ),
             ListView.builder(
               shrinkWrap: true,
               itemCount: 5,
+              padding: EdgeInsets.symmetric(horizontal: getSize(18)),
               physics: BouncingScrollPhysics(),
               itemBuilder: (context, index) => Container(
                 alignment: Alignment.center,
                 padding: EdgeInsets.all(getSize(12)),
                 margin: EdgeInsets.symmetric(vertical: getSize(5)),
                 decoration: BoxDecoration(
+                  color: Color(0xFFD9D9D9).withOpacity(0.20),
                   borderRadius: BorderRadius.circular(getSize(10)),
                   border: Border.all(
                     color: AppColors.black.withOpacity(0.2),
@@ -325,27 +356,30 @@ class ProductDetailsView extends StatelessWidget {
   }
 
   getProductDetails({required String title, required String description}) {
-    return RichText(
-      text: TextSpan(
-        style: TextStyle(
-          fontSize: getFontSize(12),
-          color: AppColors.black.withOpacity(0.80),
-          fontFamily: 'SfPro',
-          fontWeight: FontWeight.w500,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: getSize(18)),
+      child: RichText(
+        text: TextSpan(
+          style: TextStyle(
+            fontSize: getFontSize(12),
+            color: AppColors.black.withOpacity(0.80),
+            fontFamily: 'SfPro',
+            fontWeight: FontWeight.w500,
+          ),
+          children: [
+            TextSpan(text: '$title: '),
+            TextSpan(
+              text: description,
+              style: TextStyle(
+                color: AppColors.black.withOpacity(0.60),
+                fontFamily: 'SfPro',
+                fontWeight: FontWeight.w400,
+              ),
+            )
+          ],
         ),
-        children: [
-          TextSpan(text: '$title: '),
-          TextSpan(
-            text: description,
-            style: TextStyle(
-              color: AppColors.black.withOpacity(0.60),
-              fontFamily: 'SfPro',
-              fontWeight: FontWeight.w400,
-            ),
-          )
-        ],
+        textScaler: TextScaler.linear(1),
       ),
-      textScaler: TextScaler.linear(1),
     );
   }
 }
