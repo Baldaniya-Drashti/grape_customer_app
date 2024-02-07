ab5430e7b24eb22429374df6198cbc76111c529f
maitry-iroid <maitry@iroidsolutions.com>
2024-02-07 15:47:50 +0530

    maitry :  checkout screen done

---
+++ lib/presentation/main/tabs/home/checkout/widget/get_checkout_product.dart
@@ -0,0 +1,169 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:figma_squircle/figma_squircle.dart';
+import 'package:flutter/material.dart';
+import 'package:grape_customer_app/domain/core/math_utils.dart';
+import 'package:grape_customer_app/presentation/common/widgets/base_text.dart';
+import 'package:grape_customer_app/presentation/core/styles/app_colors.dart';
+
+class GetCheckoutProductDetailWidget extends StatelessWidget {
+  const GetCheckoutProductDetailWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: getSize(10),
+        ),
+        getChooseCategoryView(),
+        getCheckoutContainer(),
+      ],
+    );
+  }
+
+  Container getCheckoutContainer() {
+    return Container(
+      padding: EdgeInsets.all(getSize(10)),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(getSize(10)),
+          bottomRight: Radius.circular(getSize(10)),
+          bottomLeft: Radius.circular(getSize(10)),
+        ),
+        border: Border.all(
+          color: AppColors.primaryOrange,
+        ),
+      ),
+      child: Stack(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: getSize(90),
+                width: getSize(90),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(getSize(10)),
+                  ),
+                ),
+                child: CachedNetworkImage(
+                  imageUrl:
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0U3avlAFpuN9Sf5PVhN3MHdXQzh6rJusL93tMQRngAnrK1k0Z9CH4hzhArR0kyV-Fm_E&usqp=CAU',
+                  placeholder: (context, url) => Container(
+                    height: getSize(80),
+                    width: getSize(60),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: SmoothBorderRadius.all(
+                        SmoothRadius(
+                          cornerRadius: getSize(4),
+                          cornerSmoothing: 1,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: getSize(8),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BaseText(
+                    text: 'Nothing Phone 1',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  SizedBox(
+                    height: getSize(8),
+                  ),
+                  getProductDetails(
+                    title: 'Colors',
+                    description: 'Black',
+                  ),
+                  SizedBox(
+                    height: getSize(6),
+                  ),
+                  getProductDetails(
+                    title: 'Size',
+                    description: '128 GB',
+                  ),
+                  SizedBox(
+                    height: getSize(6),
+                  ),
+                  getProductDetails(
+                    title: 'Quantity',
+                    description: '1',
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: BaseText(
+                text: '\$299',
+                fontSize: 18,
+                textColor: AppColors.mildBlue,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  RichText getProductDetails(
+      {required String title, required String description}) {
+    return RichText(
+      text: TextSpan(
+        style: TextStyle(
+          fontSize: getFontSize(10),
+          color: AppColors.black.withOpacity(0.80),
+          fontFamily: 'SfPro',
+          fontWeight: FontWeight.w500,
+        ),
+        children: [
+          TextSpan(text: '$title: '),
+          TextSpan(
+            text: description,
+            style: TextStyle(
+              color: AppColors.black,
+              fontFamily: 'SfPro',
+              fontWeight: FontWeight.w600,
+            ),
+          )
+        ],
+      ),
+      textScaler: TextScaler.linear(1),
+    );
+  }
+
+  getChooseCategoryView() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: getSize(10),
+        vertical: getSize(3),
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.primaryOrange,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(getSize(9)),
+          topLeft: Radius.circular(getSize(9)),
+        ),
+      ),
+      child: BaseText(
+        text: 'Choose for buying',
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        textColor: AppColors.white,
+      ),
+    );
+  }
+}
---
+++ lib/presentation/main/tabs/home/checkout/widget/shippping_address_widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:grape_customer_app/domain/core/math_utils.dart';
+import 'package:grape_customer_app/presentation/common/widgets/base_text.dart';
+import 'package:grape_customer_app/presentation/core/styles/app_colors.dart';
+
+class ShippingAddressWidget extends StatelessWidget {
+  const ShippingAddressWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: getSize(30),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            BaseText(
+              text: 'Shipping Address',
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+            ),
+            BaseText(
+              text: 'Change',
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+              textColor: AppColors.primaryOrange,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: getSize(10),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(
+            vertical: getSize(18),
+            horizontal: getSize(24),
+          ),
+          decoration: BoxDecoration(
+            color: AppColors.grey.withOpacity(0.20),
+            borderRadius: BorderRadius.circular(getSize(10)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BaseText(
+                text: 'John Andrew',
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
+              BaseText(
+                text: '+60 85285 8525',
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
+              BaseText(
+                text:
+                    '3711 Spring Hill Rd undefined Tallahassee, Nevada 52874 United States',
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+              BaseText(
+                text: 'Landmark: Near Smart Mall',
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
